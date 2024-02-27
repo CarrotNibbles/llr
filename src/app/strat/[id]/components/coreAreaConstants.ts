1ad6f47aff27daba332d22a02e68028d28a4ac3c
coinmoles <andrewpark421@gmail.com>
2024-02-27 22:38:43 +0900

    Added zoom-in support to EditColumns (incomplete)

--- src/app/strat/[id]/components/coreAreaConstants.ts
+++ src/app/strat/[id]/components/coreAreaConstants.ts
@@ -1,3 +1,6 @@
+import { useZoomState } from '@/lib/states';
+import { getZoom } from '@/lib/utils';
+
 export const columnWidth = 6;
 export const columnWidthLarge = 10;
 
@@ -6,5 +9,12 @@ export type SkillTemp = {
   id: string;
 };
 
-export const pixelPerSecTemp = 5;
-export const raidDurationTemp = 600;
+export const timeStep = 0.5;
+export const usePixelPerFrame = () => {
+  const [zoomState, _] = useZoomState();
+  const pixelPerFrameDefault = 0.1;
+
+  return pixelPerFrameDefault * getZoom(zoomState);
+};
+
+export const raidDurationTemp = 600 * 60;
