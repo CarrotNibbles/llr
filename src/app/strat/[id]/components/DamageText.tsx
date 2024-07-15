75c61b26d06e990649b1c8ff1a442dee09a21859
hareen <me@hareen.io>
2024-07-15 16:37:55 +0900

    handle damage option upsert

--- src/app/strat/[id]/components/DamageText.tsx
+++ src/app/strat/[id]/components/DamageText.tsx
@@ -1,17 +1,34 @@
 'use client';
 
+import { useStratSyncStore } from '@/components/providers/StratSyncStoreProvider';
 import type { StrategyDataType } from '@/lib/queries/server';
-import type { ArrayElement } from '@/lib/utils';
+import { cn, getRawRole, type ArrayElement } from '@/lib/utils';
 
 type DamageTextProps = {
+  damageId: string;
   defaultDamage: number;
   currentDamage: number;
   primaryTarget?: string;
   numShared?: number;
 };
 
+const useTank: () => [string, string] | [string, undefined] | [undefined, undefined] = () => {
+  const {
+    strategyData: { strategy_players },
+  } = useStratSyncStore((state) => state);
+
+  const tanks = strategy_players
+    .filter((player) => player.job && getRawRole(player.job) === 'Tank')
+    .map((player) => player.id);
+
+  if (tanks.length === 0) return [undefined, undefined];
+  if (tanks.length === 1) return [tanks[0], undefined];
+  return [tanks[0], tanks[1]];
+};
+
 const BothTankBuster = (props: DamageTextProps) => {
   const { defaultDamage, currentDamage } = props;
+
   return (
     <>
       <div className="space-x-1 pr-6">
@@ -25,20 +42,46 @@ const BothTankBuster = (props: DamageTextProps) => {
 
 const SingleTankBuster = (props: DamageTextProps) => {
   const { defaultDamage, currentDamage, primaryTarget } = props;
+
+  const [mainTank, offTank] = useTank();
+  const { upsertDamageOption, elevated } = useStratSyncStore((state) => state);
+
+  const activeOption = primaryTarget === offTank ? 1 : 0;
+  const cursorStyle = elevated ? 'cursor-pointer' : 'cursor-not-allowed';
+
   return (
     <>
       <div className="space-x-1 pr-6">
-        {primaryTarget === 'T2' ? (
-          <>
-            <span className="text-muted-foreground">T1</span>
-            <span className="font-bold">T2</span>
-          </>
-        ) : (
-          <>
-            <span className="font-bold">T1</span>
-            <span className="text-muted-foreground">T2</span>
-          </>
-        )}
+        {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+        <span
+          className={cn(activeOption === 0 ? 'font-bold' : 'text-muted-foreground', cursorStyle)}
+          onClick={() => {
+            upsertDamageOption(
+              {
+                damage: props.damageId,
+                primaryTarget: mainTank,
+              },
+              false,
+            );
+          }}
+        >
+          T1
+        </span>
+        {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+        <span
+          className={cn(activeOption === 1 ? 'font-bold' : 'text-muted-foreground', cursorStyle)}
+          onClick={() => {
+            upsertDamageOption(
+              {
+                damage: props.damageId,
+                primaryTarget: offTank,
+              },
+              false,
+            );
+          }}
+        >
+          T2
+        </span>
       </div>
       <span className="tabular-nums font-bold">{currentDamage}</span>
       <span className="text-muted-foreground tabular-nums text-xs my-auto">{defaultDamage}</span>
@@ -48,28 +91,60 @@ const SingleTankBuster = (props: DamageTextProps) => {
 
 const ShareTankBuster = (props: DamageTextProps) => {
   const { defaultDamage, currentDamage, primaryTarget } = props;
+
+  const [mainTank, offTank] = useTank();
+  const { upsertDamageOption, elevated } = useStratSyncStore((state) => state);
+
+  const activeOption = primaryTarget === undefined ? 0 : primaryTarget === mainTank ? 1 : 2;
+  const cursorStyle = elevated ? 'cursor-pointer' : 'cursor-not-allowed';
+
   return (
     <>
       <div className="space-x-1 pr-6">
-        {primaryTarget === null ? (
-          <>
-            <span className="font-bold">T1+T2</span>
-            <span className="text-muted-foreground">T1</span>
-            <span className="text-muted-foreground">T2</span>
-          </>
-        ) : primaryTarget === 'T1' ? (
-          <>
-            <span className="text-muted-foreground">T1+T2</span>
-            <span className="font-bold">T1</span>
-            <span className="text-muted-foreground">T2</span>
-          </>
-        ) : (
-          <>
-            <span className="text-muted-foreground">T1+T2</span>
-            <span className="text-muted-foreground">T1</span>
-            <span className="font-bold">T2</span>
-          </>
-        )}
+        {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+        <span
+          className={cn(activeOption === 0 ? 'font-bold' : 'text-muted-foreground', cursorStyle)}
+          onClick={() => {
+            upsertDamageOption(
+              {
+                damage: props.damageId,
+              },
+              false,
+            );
+          }}
+        >
+          T1+T2
+        </span>
+        {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+        <span
+          className={cn(activeOption === 1 ? 'font-bold' : 'text-muted-foreground', cursorStyle)}
+          onClick={() => {
+            upsertDamageOption(
+              {
+                damage: props.damageId,
+                primaryTarget: mainTank,
+              },
+              false,
+            );
+          }}
+        >
+          T1
+        </span>
+        {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+        <span
+          className={cn(activeOption === 2 ? 'font-bold' : 'text-muted-foreground', cursorStyle)}
+          onClick={() => {
+            upsertDamageOption(
+              {
+                damage: props.damageId,
+                primaryTarget: offTank,
+              },
+              false,
+            );
+          }}
+        >
+          T2
+        </span>
       </div>
       <span className="tabular-nums font-bold">{currentDamage}</span>
       <span className="text-muted-foreground tabular-nums text-xs my-auto">{defaultDamage}</span>
@@ -105,6 +180,7 @@ const RaidWide = (props: DamageTextProps) => {
 
 const ShareHalfRaidWide = (props: DamageTextProps) => {
   const { defaultDamage, currentDamage, numShared } = props;
+
   return (
     <>
       <div className="space-x-1 pr-6">
@@ -126,7 +202,24 @@ const ShareHalfRaidWide = (props: DamageTextProps) => {
   );
 };
 
+// This is a fallback component for when the damage type is unknown
 const Unknown = () => <div className="space-x-1 pr-6">머지 버그인듯</div>;
+
+const componentSelector = (target: 'Raidwide' | 'Tankbuster', numTargets: number, maxShared: number) => {
+  if (target === 'Tankbuster') {
+    if (numTargets === 1 && maxShared === 1) return SingleTankBuster;
+    if (numTargets === 1 && maxShared === 2) return ShareTankBuster;
+    if (numTargets === 2) return BothTankBuster;
+  }
+
+  if (target === 'Raidwide') {
+    if (numTargets === 1 && maxShared === 8) return ShareAllRaidWide;
+    if (numTargets === 2 && maxShared === 4) return ShareHalfRaidWide;
+    if (numTargets === 8 && maxShared === 1) return RaidWide;
+  }
+
+  return Unknown;
+};
 
 export const DamageText = ({
   damages,
@@ -136,28 +229,19 @@ export const DamageText = ({
   return (
     <>
       {damages.map((damage) => {
-        if (damage.target === 'Tankbuster') {
-          if (damage.num_targets === 1 && damage.max_shared === 1)
-            return <SingleTankBuster key={damage.id} defaultDamage={damage.combined_damage} currentDamage={90000} />;
-          if (damage.num_targets === 1 && damage.max_shared === 2)
-            return <ShareTankBuster key={damage.id} defaultDamage={damage.combined_damage} currentDamage={90000} />;
-          if (damage.num_targets === 2)
-            return <BothTankBuster key={damage.id} defaultDamage={damage.combined_damage} currentDamage={90000} />;
-          return <Unknown key={damage.id} />;
-        }
+        const textProps = {
+          damageId: damage.id,
+          defaultDamage: damage.combined_damage,
+          currentDamage: 90000,
+          primaryTarget: damage.strategy_damage_options?.[0]?.primary_target ?? undefined,
+          numShared: damage.strategy_damage_options?.[0]?.num_shared ?? undefined,
+        };
 
-        if (damage.target === 'Raidwide') {
-          if (damage.num_targets === 1 && damage.max_shared === 8)
-            return <ShareAllRaidWide key={damage.id} defaultDamage={damage.combined_damage} currentDamage={90000} />;
-          if (damage.num_targets === 2 && damage.max_shared === 4)
-            return <ShareHalfRaidWide key={damage.id} defaultDamage={damage.combined_damage} currentDamage={90000} />;
-          if (damage.num_targets === 8 && damage.max_shared === 1)
-            return <RaidWide key={damage.id} defaultDamage={damage.combined_damage} currentDamage={900000} />;
+        const key = `damagetext-${damage.id}`;
 
-          return <Unknown key={damage.id} />;
-        }
+        const TextComponent = componentSelector(damage.target, damage.num_targets, damage.max_shared);
 
-        return <Unknown key={damage.id} />;
+        return <TextComponent key={key} {...textProps} />;
       })}
     </>
   );
