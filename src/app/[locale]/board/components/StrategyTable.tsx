96e21b83a2d33182323e865363228b8f94f4dfd4
coinmoles <andrewpark421@gmail.com>
2024-10-12 15:11:16 +0900

    Add suspense for strategy table

--- src/app/[locale]/board/components/StrategyTable.tsx
+++ src/app/[locale]/board/components/StrategyTable.tsx
@@ -1,81 +1,25 @@
+'use server';
+
 import { JobIcon } from '@/components/JobIcon';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import type { StrategiesDataType } from '@/lib/queries/server';
-import { type ArrayElement, cn, getOrderedRole } from '@/lib/utils';
+import { buildStrategiesDataQuery } from '@/lib/queries/server';
+import { createClient } from '@/lib/supabase/server';
+import { cn, getOrderedRole, rangeInclusive } from '@/lib/utils';
 import Link from 'next/link';
 import type React from 'react';
 import { ModifiedTime } from './ModifiedTime';
-
-type StrategyRowProps = Readonly<
-  React.HTMLAttributes<HTMLTableRowElement> & {
-    strategyData: ArrayElement<StrategiesDataType>;
-  }
->;
-
-export const StrategyRow: React.FC<StrategyRowProps> = async ({ strategyData, className, ...props }) => (
-  <TableRow className={className} {...props}>
-    <TableCell className="p-0 h-0">
-      <Link href={`/strat/${strategyData.id}`} className="w-full h-full flex items-center">
-        <div className="flex flex-col pl-4 py-4 pr-2">
-          <h2 className="text-base md:text-lg font-bold">{strategyData.name}</h2>
-          <div className="text-xs md:text-sm text-muted-foreground">{strategyData.raids?.name}</div>
-          {strategyData.strategy_players.length !== 0 && (
-            <div>
-              <div className="inline-grid grid-cols-4 sm:grid-cols-8 gap-1 mt-2">
-                {strategyData.strategy_players.map((player) => (
-                  <JobIcon
-                    job={player.job}
-                    role={getOrderedRole(player.job, player.order)}
-                    key={player.id}
-                    className="aspect-square w-6 h-6"
-                  />
-                ))}
-              </div>
-            </div>
-          )}
-        </div>
-      </Link>
-    </TableCell>
-    <TableCell className="p-0 w-0 h-0 hidden md:table-cell">
-      <Link href={`/strat/${strategyData.id}`} className="w-full h-full flex items-center">
-        <div className="px-2 py-4">Mario Mario</div>
-      </Link>
-    </TableCell>
-    <TableCell className="p-0 w-0 h-0">
-      <Link href={`/strat/${strategyData.id}`} className="w-full h-full flex justify-center items-center">
-        <div className="px-2 py-4">{`${strategyData.version}.${strategyData.subversion}`}</div>
-      </Link>
-    </TableCell>
-    <TableCell className="p-0 w-0 h-0">
-      <Link href={`/strat/${strategyData.id}`} className="w-full h-full flex justify-center items-center">
-        <div className="px-2 py-4">
-          {strategyData.like_counts === null
-            ? 0
-            : strategyData.like_counts.anon_likes + strategyData.like_counts.user_likes}
-        </div>
-      </Link>
-    </TableCell>
-    <TableCell className="p-0 w-0 h-0  hidden md:table-cell">
-      <Link href={`/strat/${strategyData.id}`} className="w-full h-full flex justify-center items-center">
-        <div className="px-2 py-4 flex justify-center items-center">
-          <ModifiedTime createdAt={strategyData.created_at} modifiedAt={strategyData.modified_at} />
-        </div>
-      </Link>
-    </TableCell>
-  </TableRow>
-);
+import { Skeleton } from '@/components/ui/skeleton';
+import { Suspense } from 'react';
+import { TextSkeleton } from '@/components/TextSkeleton';
 
 type StrategyTableProps = Readonly<
   React.HTMLAttributes<HTMLTableElement> & {
-    strategiesData: StrategiesDataType;
+    page: number;
+    limit: number;
   }
 >;
 
-export const StrategyTable: React.FC<StrategyTableProps> = async ({
-  strategiesData: strategyDatas,
-  className,
-  ...props
-}) => {
+export const StrategyTable: React.FC<StrategyTableProps> = async ({ page, limit, className, ...props }) => {
   return (
     <Table className={cn(className, 'border-b')} {...props}>
       <TableHeader>
@@ -93,11 +37,132 @@ export const StrategyTable: React.FC<StrategyTableProps> = async ({
           </TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody>
-        {strategyDatas.map((strategyData) => (
-          <StrategyRow key={strategyData.id} strategyData={strategyData} />
-        ))}
-      </TableBody>
+      <Suspense fallback={<StrategyTableBodySkeleton limit={limit} />}>
+        <StrategyTableBody page={page} limit={limit} />
+      </Suspense>
     </Table>
   );
 };
+
+type StrategyTableBodyProps = Readonly<
+  React.HTMLAttributes<HTMLTableSectionElement> & {
+    page: number;
+    limit: number;
+  }
+>;
+
+const StrategyTableBody: React.FC<StrategyTableBodyProps> = async ({ page, limit, className, ...props }) => {
+  const supabase = createClient();
+  const { data: strategiesData, error: strategiesDataQueryError } = await buildStrategiesDataQuery(
+    supabase,
+    page,
+    limit,
+  );
+  if (strategiesDataQueryError || strategiesData === null) throw strategiesDataQueryError;
+  return (
+    <TableBody className={className} {...props}>
+      {strategiesData.map((strategyData) => (
+        <TableRow key={strategyData.id}>
+          <TableCell className="p-0 h-0">
+            <Link href={`/strat/${strategyData.id}`} className="w-full h-full flex items-center">
+              <div className="flex flex-col pl-4 py-4 pr-2">
+                <h2 className="text-base md:text-lg font-bold">{strategyData.name}</h2>
+                <div className="text-xs md:text-sm text-muted-foreground">{strategyData.raids?.name}</div>
+                {strategyData.strategy_players.length !== 0 && (
+                  <div>
+                    <div className="inline-grid grid-cols-4 sm:grid-cols-8 gap-1 mt-2">
+                      {strategyData.strategy_players.map((player) => (
+                        <JobIcon
+                          job={player.job}
+                          role={getOrderedRole(player.job, player.order)}
+                          key={player.id}
+                          className="aspect-square w-6 h-6"
+                        />
+                      ))}
+                    </div>
+                  </div>
+                )}
+              </div>
+            </Link>
+          </TableCell>
+          <TableCell className="p-0 w-0 h-0 hidden md:table-cell">
+            <Link href={`/strat/${strategyData.id}`} className="w-full h-full flex items-center">
+              <div className="px-2 py-4">Mario Mario</div>
+            </Link>
+          </TableCell>
+          <TableCell className="p-0 w-0 h-0">
+            <Link href={`/strat/${strategyData.id}`} className="w-full h-full flex justify-center items-center">
+              <div className="px-2 py-4">{`${strategyData.version}.${strategyData.subversion}`}</div>
+            </Link>
+          </TableCell>
+          <TableCell className="p-0 w-0 h-0">
+            <Link href={`/strat/${strategyData.id}`} className="w-full h-full flex justify-center items-center">
+              <div className="px-2 py-4">
+                {strategyData.like_counts === null
+                  ? 0
+                  : strategyData.like_counts.anon_likes + strategyData.like_counts.user_likes}
+              </div>
+            </Link>
+          </TableCell>
+          <TableCell className="p-0 w-0 h-0  hidden md:table-cell">
+            <Link href={`/strat/${strategyData.id}`} className="w-full h-full flex justify-center items-center">
+              <div className="px-2 py-4 flex justify-center items-center">
+                <ModifiedTime createdAt={strategyData.created_at} modifiedAt={strategyData.modified_at} />
+              </div>
+            </Link>
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  );
+};
+
+type StrategyTableBodySkeletonProps = Readonly<
+  React.HTMLAttributes<HTMLTableSectionElement> & {
+    limit: number;
+  }
+>;
+
+const StrategyTableBodySkeleton: React.FC<StrategyTableBodySkeletonProps> = ({ limit, className, ...props }) => (
+  <TableBody className={className} {...props}>
+    {rangeInclusive(1, limit).map((index) => (
+      <TableRow key={index}>
+        <TableCell className="p-0 h-0">
+          <div className="w-full h-full flex items-center">
+            <div className="w-full flex flex-col pl-4 py-4 pr-2">
+              <TextSkeleton textSize="base" mdTextSize="lg" className="w-1/2" />
+              <TextSkeleton textSize="xs" mdTextSize="sm" className="w-1/3" />
+              <div>
+                <div className="inline-grid grid-cols-4 sm:grid-cols-8 gap-1 mt-2">
+                  {rangeInclusive(1, 8).map((index) => (
+                    <Skeleton key={index} className="aspect-square w-6 h-6" />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </TableCell>
+        <TableCell className="p-0 w-0 h-0 hidden md:table-cell">
+          <div className="w-full h-full flex items-center">
+            {/* <TextSkeleton textSize="sm" className="w-1/2" /> */}
+          </div>
+        </TableCell>
+        <TableCell className="p-0 w-0 h-0">
+          <div className="w-full h-full flex justify-center items-center">
+            {/* <TextSkeleton textSize="sm" className="w-1/2" /> */}
+          </div>
+        </TableCell>
+        <TableCell className="p-0 w-0 h-0">
+          <div className="w-full h-full flex justify-center items-center">
+            {/* <TextSkeleton textSize="sm" className="w-1/2" /> */}
+          </div>
+        </TableCell>
+        <TableCell className="p-0 w-0 h-0  hidden md:table-cell">
+          <div className="w-full h-full flex justify-center items-center">
+            {/* <TextSkeleton textSize="sm" className="w-1/2" /> */}
+          </div>
+        </TableCell>
+      </TableRow>
+    ))}
+  </TableBody>
+);
--- src/app/[locale]/board/page.tsx
+++ src/app/[locale]/board/page.tsx
@@ -2,14 +2,14 @@
 
 import { buildRaidsDataQuery, buildStrategiesDataQuery, buildStrategyCountQuery } from '@/lib/queries/server';
 import { createClient } from '@/lib/supabase/server';
-import { buildURL, DEFAULT_LIMIT, tryParseInt } from '@/lib/utils';
-import { BoardHeader } from './components/BoardHeader';
-import { BoardSubHeader } from './components/BoardSubHeader';
-import { StrategyTable } from './components/StrategyTable';
+import { DEFAULT_LIMIT, buildURL, tryParseInt } from '@/lib/utils';
 import { redirect } from 'next/navigation';
+import { BoardHeader } from './components/BoardHeader';
 import { BoardPagination } from './components/BoardPagination';
-import { Button } from '@/components/ui/button';
+import { BoardSubHeader } from './components/BoardSubHeader';
 import { LimitCombobox } from './components/LimitCombobox';
+import { StrategyTable } from './components/StrategyTable';
+import { Suspense } from 'react';
 
 type BoardPageProps = Readonly<{
   params: { locale: string };
@@ -36,20 +36,13 @@ export default async function BoardPage({ params: { locale }, searchParams }: Bo
   const { data: raidsData, error: raidsDataQueryError } = await buildRaidsDataQuery(supabase);
   if (raidsDataQueryError || raidsData === null) throw raidsDataQueryError;
 
-  const { data: strategiesData, error: strategiesDataQueryError } = await buildStrategiesDataQuery(
-    supabase,
-    page,
-    limit,
-  );
-  if (strategiesDataQueryError || strategiesData === null) throw strategiesDataQueryError;
-
   return (
     <div className="flex flex-col items-center">
       <BoardHeader />
       <div className="flex flex-col w-full max-w-screen-xl px-4 py-1">
         <BoardSubHeader raidsData={raidsData} />
         <div className="px-4 mt-2 mb-8">
-          <StrategyTable strategiesData={strategiesData} />
+          <StrategyTable page={page} limit={limit} />
           <div className="w-full flex flex-col-reverse md:grid md:grid-cols-3 gap-y-2 mt-2">
             <div />
             <BoardPagination currentPage={page} maxPage={maxPage} />
