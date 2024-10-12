2658916f4fec816ca721dc7e831263e4acb09665
coinmoles <andrewpark421@gmail.com>
2024-10-12 20:38:16 +0900

    Make useMediaQuery set on initial render

--- src/lib/hooks/useMediaQuery.ts
+++ src/lib/hooks/useMediaQuery.ts
@@ -4,6 +4,7 @@ export function useMediaQuery(query: string) {
   const [matches, setMatches] = React.useState(false);
   React.useEffect(() => {
     const matchQueryList = window.matchMedia(query);
+    setMatches(matchQueryList.matches);
     function handleChange(e: MediaQueryListEvent) {
       setMatches(e.matches);
     }
