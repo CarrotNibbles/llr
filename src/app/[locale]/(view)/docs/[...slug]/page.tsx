c709f86187abe7b6581c6cbc90573f067987cfba
hareeen <me@hareen.io>
2024-11-05 21:04:07 +0900

    chore: throw 404 when doc not found

--- src/app/[locale]/(view)/docs/[...slug]/page.tsx
+++ src/app/[locale]/(view)/docs/[...slug]/page.tsx
@@ -1,16 +1,21 @@
 import { ProseArticle } from '@/components/ProseArticle';
 import { getLocale } from 'next-intl/server';
+import { notFound } from 'next/navigation';
 
 export default async function DocumentPage({ params: { slug } }: { params: { slug: string[] } }) {
   const locale = await getLocale();
 
-  const Doc = (await import(`./contents/${locale}/${slug.join('/')}.mdx`)).default;
+  try {
+    const Document = (await import(`./contents/${locale}/${slug.join('/')}.mdx`)).default;
 
-  return (
-    <div className="flex flex-col w-full max-w-screen-md px-6 md:py-6 py-3">
-      <ProseArticle>
-        <Doc />
-      </ProseArticle>
-    </div>
-  );
+    return (
+      <div className="flex flex-col w-full max-w-screen-md px-6 md:py-6 py-3">
+        <ProseArticle>
+          <Document />
+        </ProseArticle>
+      </div>
+    );
+  } catch {
+    notFound();
+  }
 }
